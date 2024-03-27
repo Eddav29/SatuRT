498092a749a50ea7ec85de7e173e48cbad279eb3
raihanachmad8 <raihanachmad8@gmail.com>
2024-03-27 15:02:20 +0700

    feat(layout): Add Guest Layout

---
+++ resources/views/components/nav-button.blade.php
@@ -0,0 +1,9 @@
+@props(['theme'])
+
+@php
+    $classes = ($theme == 'light') ? 'bg-gray-800 text-white' : 'bg-white text-gray-800';
+@endphp
+
+<a draggable="false" {{ $attributes->merge(['class' => 'select none inline-flex items-center px-8 py-3 md:px-10 md:py-4 rounded-xl font-semibold text-sm md:text-md leading-5 transition duration-150 ease-in-out ' . $classes]) }}>
+        {{ $slot }}
+</a>
---
+++ resources/views/pages/landing-page/home/index.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 ">
+            {{ __("You're logged in!") }}
+        </div>
+    </div>
+</x-guest-layout>
