9ca5dcfee21a2d1021c59a963b1b97f38366fb8b
Vunky Himawan <tingkerbeii44@gmail.com>
2024-06-03 11:19:24 +0700

    feat(middleware): add only guest can access home page

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkRole' => \App\Http\Middleware\CheckRole::class,
+        'onlyGuest' => \App\Http\Middleware\GuestMiddleware::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -35,13 +35,13 @@ use App\Http\Controllers\FileController;
 */
 
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('onlyGuest');
 
-Route::get('berita', [NewsController::class, 'index'])->name('berita');
-Route::get('berita/{id}', [NewsController::class, 'show'])->name('berita-detail');
+Route::get('berita', [NewsController::class, 'index'])->name('berita')->middleware('onlyGuest');
+Route::get('berita/{id}', [NewsController::class, 'show'])->name('berita-detail')->middleware('onlyGuest');
 
-Route::get('usaha', [BusinessController::class, 'index'])->name('usaha');
-Route::get('usaha/{id}', [BusinessController::class, 'show'])->name('usaha-detail');
+Route::get('usaha', [BusinessController::class, 'index'])->name('usaha')->middleware('onlyGuest');
+Route::get('usaha/{id}', [BusinessController::class, 'show'])->name('usaha-detail')->middleware('onlyGuest');
 
 require __DIR__ . '/auth.php';
 
@@ -133,15 +133,15 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/inventaris/peminjaman/selesaikan/{id}', [InventarisPeminjamanController::class, 'selesaikan'])->name('inventaris.peminjaman.selesaikan');
 
         Route::resource('data-akun/penduduk', CitizenAccountController::class)
-        ->names([
-            'index' => 'data-akun.index',
-            'create' => 'data-akun.create',
-            'store' => 'data-akun.store',
-            'show' => 'data-akun.show',
-            'edit' => 'data-akun.edit',
-            'update' => 'data-akun.update',
-            'destroy' => 'data-akun.destroy'
-        ]);
+            ->names([
+                'index' => 'data-akun.index',
+                'create' => 'data-akun.create',
+                'store' => 'data-akun.store',
+                'show' => 'data-akun.show',
+                'edit' => 'data-akun.edit',
+                'update' => 'data-akun.update',
+                'destroy' => 'data-akun.destroy'
+            ]);
     });
 
     Route::middleware('checkRole:Ketua RT,Penduduk')->group(function () {
