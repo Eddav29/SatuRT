60846a5153cbff7e662412338cd901c76fadb701
Vunky Himawan <tingkerbeii44@gmail.com>
2024-06-05 12:36:55 +0700

    fix(umkm): fix description field

--- resources/views/pages/umkm/show.blade.php
+++ resources/views/pages/umkm/show.blade.php
@@ -14,8 +14,7 @@
                 </div>
             </section>
             <section>
-                <div
-                    class="p-6 mx-auto max-w-screen-2xl md:p-6 2xl:p-6 flex flex-col md:grid md:grid-cols-2 gap-y-5">
+                <div class="p-6 mx-auto max-w-screen-2xl md:p-6 2xl:p-6 flex flex-col md:grid md:grid-cols-2 gap-y-5">
                     <div>
                         <h5 class="font-semibold">Pemilik</h5>
                         <p>{{ $umkm->penduduk->nama }}</p>
@@ -46,24 +45,26 @@
                     </div>
                     <div class="md:col-span-2 md:h-auto">
                         <h5 class="font-semibold">Surat Izin Usaha</h5>
-                        @if($umkm->lisence_image_url)
-                        <x-image-preview :file="is_null($umkm->lisence_image_url)
-                            ? null
-                            : (strpos($umkm->lisence_image_url, 'http') === false
-                                ? route('storage.lisence', $umkm->lisence_image_url)
-                                : $umkm->lisence_image_url)" />
+                        @if ($umkm->lisence_image_url)
+                            <x-image-preview :file="is_null($umkm->lisence_image_url)
+                                ? null
+                                : (strpos($umkm->lisence_image_url, 'http') === false
+                                    ? route('storage.lisence', $umkm->lisence_image_url)
+                                    : $umkm->lisence_image_url)" />
                         @else
-                        <p>Tidak ada surat izin usaha</p>
+                            <p>Tidak ada surat izin usaha</p>
                         @endif
                     </div>
                     <div class="md:col-span-2">
                         <h5 class="font-semibold">Thumbnail</h5>
-                        @if($umkm->thumbnail_url)
-                        <x-image-preview :file="is_null($umkm->thumbnail_url) ? null : (strpos($umkm->thumbnail_url, 'http') === false
-                            ? route('public', $umkm->thumbnail_url)
-                            : $umkm->thumbnail_url)" />
+                        @if ($umkm->thumbnail_url)
+                            <x-image-preview :file="is_null($umkm->thumbnail_url)
+                                ? null
+                                : (strpos($umkm->thumbnail_url, 'http') === false
+                                    ? route('public', $umkm->thumbnail_url)
+                                    : $umkm->thumbnail_url)" />
                         @else
-                        <p>Tidak ada thumbnail</p>
+                            <p>Tidak ada thumbnail</p>
                         @endif
                     </div>
                     <div class="md:col-span-2">
@@ -92,4 +93,79 @@
         </div>
     </div>
     {{-- Content End --}}
+
+    @push('styles')
+        <style>
+            #content h2 {
+                display: block;
+                font-size: 1.5em;
+                font-weight: bold;
+            }
+
+            #content h3 {
+                display: block;
+                font-size: 1.33em;
+                font-weight: bold;
+            }
+
+            #content h4 {
+                display: block;
+                font-size: 1.17em;
+                font-weight: bold;
+            }
+
+            #content ol {
+                display: block;
+                list-style-type: decimal;
+                padding-left: 40px;
+            }
+
+            #content ul {
+                display: block;
+                list-style-type: disc;
+                padding-left: 40px;
+            }
+
+            #content a {
+                color: rgb(59 130 246);
+                text-decoration: underline;
+                background-color: transparent;
+            }
+
+            #content blockquote {
+                padding: 1rem;
+                margin: 1rem 0;
+                border-left: 4px solid rgb(209 213 219);
+            }
+
+            #content table {
+                table-layout: auto;
+                width: 100%;
+                font-size: 0.875rem;
+                line-height: 1.25rem;
+                text-align: left;
+                color: #0B1215;
+            }
+
+            #content table th {
+                background-color: rgb(229 231 235);
+                padding: 0.75rem;
+                border: 1px solid rgb(209 213 219);
+            }
+
+            #content table td {
+                border: 1px solid rgb(209 213 219);
+                padding: 0.75rem;
+            }
+
+            #content blockquote p {
+                font-size: 1rem;
+                line-height: 1.25rem;
+                font-style: italic;
+                font-weight: 500;
+                line-height: 1.625;
+                color: #0B1215;
+            }
+        </style>
+    @endpush
 </x-app-layout>
